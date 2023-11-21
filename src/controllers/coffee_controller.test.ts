7f97f3c84d7bb94c5688efce701f9bc766581ea3
Jennifer Sivapalan <944375+jennysivapalan@users.noreply.github.com>
2023-11-21 14:31:50 +0000

    test: adds tests for a different parameter and no parameter to the /coffee endpoint

--- src/controllers/coffee_controller.test.ts
+++ src/controllers/coffee_controller.test.ts
@@ -1,8 +1,29 @@
 import request from "supertest";
 import { app } from "../app";
 
-test("GET /coffee should return correct object", async () => {
+test("GET /coffee should return correct object for Latte", async () => {
   const res = await request(app).get("/coffee").query({ coffeeName: "Latte" });
+  expect(res.statusCode).toEqual(200);
+  expect(res.body).toEqual({
+    drinkType: "Coffee",
+    name: "Latte",
+  });
+});
+
+test("GET /coffee should return correct object for Flat White", async () => {
+  const res = await request(app)
+    .get("/coffee")
+    .query({ coffeeName: "Flat White" });
+  expect(res.statusCode).toEqual(200);
+  expect(res.body).toEqual({
+    drinkType: "Coffee",
+    name: "Flat White",
+  });
+});
+
+test("GET /coffee should return Latte when no parameter specified", async () => {
+  const res = await request(app).get("/coffee");
+
   expect(res.statusCode).toEqual(200);
   expect(res.body).toEqual({
     drinkType: "Coffee",
